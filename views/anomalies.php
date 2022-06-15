bb836aa6b4f8f5d5f8c23a163a4440c3de5cdb59
Oussama Elkhaldaoui <93930380+elkhaldaoui@users.noreply.github.com>
2022-06-15 14:33:10 +0100

    Update anomalies.php

--- views/anomalies.php
+++ views/anomalies.php
@@ -4,27 +4,32 @@
     <div class="d-grid gap-2 m-3">
       <button href="" class="btn btn-primary" type="button">Add New Anomalie</button>
     </div>
-<div class="d-grid gap-2 m-3">
+<!-- container -->
+<div class="container overflow-auto">
+<div class="row m-3">
     <!-- cards -->
-    <div>
-      <div class="card" style="width: 18rem;">
-      <img src="views/assests/img/moteur-electrique.jpg" class="card-img-top" alt="...">
-      <div class="card">
-      <div class="card-body">
-      <h5 class="card-title">Special title treatment</h5>
-      <p class="card-text lh-1">
-        -With supporting text below as a natural lead-in to additional content.
-        -With supporting text below as a natural lead-in to additional content.
-      </p>
-      <label for="date">2022</label>
-      <button type="button" class="btn btn-success btn-sm">Update</button>
-      <button type="button" class="btn btn-danger btn-sm">Delete</button>
-      </div>
-      </div>
+    <div class="card m-3" style="max-width: 540px;">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="views/assests/img/moteur-electrique.jpg" class="img-fluid rounded-start" alt="image">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted"><b> Last updated </b> 3 mins ago</small></p>
+            <div class=" d-flex">
+              <a href="#" class="btn btn-success">Update</a>
+              <a href="#" class="btn btn-danger mx-1">Delete</a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
     <!-- cards -->
 </div>
+</div>
+<!-- container -->
 <!-- page content -->
 
 
